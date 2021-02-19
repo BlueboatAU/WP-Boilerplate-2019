556134f41f97d20e0a92acbcf475cc36ff8776ed
Guy Aldous <guy@blueboat.com.au>
2021-02-19 16:22:38 +1100

    final codekit config changes

--- wp-content/themes/blueboat-boilerplate/src/js/app.js
+++ wp-content/themes/blueboat-boilerplate/src/js/app.js
@@ -1,27 +1,31 @@
 import './_app/polyfils'
 import { waitForFinalEvent } from './_app/helpers.js'
 
-const test=() => console.log('welcome!')
-
-// cont donk = 'donk';
+const test = () => console.log('welcome!')
 
 // run all init scripts
 const loadHandler = () => {
+
   console.log('doc loaded')
+
 }
 document.addEventListener('DOMContentLoaded', loadHandler)
 
 // run all scripts on resize
 const resizeHandler = () => waitForFinalEvent(() => {
+
   console.log('window resize')
+
 }, 500, 'dont resize again')
 window.addEventListener('resize', resizeHandler)
 
 // event bubbling click handler
 const clickHandler = (event) => {
+
   // click checker
   if (event.target.matches('p')) {
     console.log('click paragraph')
   }
+  
 }
 document.addEventListener('click', clickHandler, false)
